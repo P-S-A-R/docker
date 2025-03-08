6574c4231610c0a1f926567480127746d6b3d4e8
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2025-03-08 18:32:59 +0100

    project

--- docker.tf
+++ docker.tf
@@ -13,3 +13,36 @@ resource "aws_instance" "this" {
     Name    = "docker"
   }
 }
+
+resource "aws_security_group" "allow_all_docker" {
+  name        = "allow_all_docker"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+output "docker_ip" {
+  value       = aws_instance.this.public_ip
+}
