1b8734afd5ab4cbfa4198f6f8d404879a35da689
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2025-03-08 17:58:23 +0100

    project

---
+++ docker.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "this" {
+  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
+  instance_type          = "t3.micro"
+
+  # 20GB is not enough
+  root_block_device {
+    volume_size = 50  # Set root volume size to 50GB
+    volume_type = "gp3"  # Use gp3 for better performance (optional)
+  }
+  user_data = file("docker.sh")
+  tags = {
+    Name    = "docker"
+  }
+}
